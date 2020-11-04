1dccae71435a772409eaa74c3ebc6b3d3c0da1fb
nandanmathure@gmail.com <nandanmathure@gmail.com>
2020-11-04 06:51:53 -0500

    Baseline Code

---
+++ src/ConsoleWriter/ColorConsole.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleWriter
+{
+    public class ColorConsole
+    {
+        public void Write(string value, ConsoleColor foregroundColor = default, ConsoleColor backgroundColor = default)
+        {
+            var originalForegroundColor = Console.ForegroundColor;
+            var originalBackgroundColor = Console.BackgroundColor;
+            Console.ForegroundColor = foregroundColor != default ? foregroundColor : originalForegroundColor;
+            Console.BackgroundColor = backgroundColor != default ? backgroundColor : originalBackgroundColor;
+            Console.WriteLine(value);
+            Console.ForegroundColor = originalForegroundColor;
+            Console.BackgroundColor = originalBackgroundColor;
+        }
+    }
+}
